f57d283c35ad189d13443f8f087456fb5840c7a6
zax <zax1314157@163.com>
2019-07-26 08:42:32 +0800

    我用小鸡鸡剔牙

---
+++ src/main/java/com/qiyou/com/zax/vo/smallJJ.java
@@ -0,0 +1,24 @@
+package com.qiyou.com.zax.vo;
+
+import java.util.ArrayList;
+
+/**
+ * @author zax
+ * @program: 20k_soeasy
+ * @description: 小鸡鸡
+ * @create: 2019-07-26 08:40
+ */
+public class smallJJ {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("小鸡鸡");
+        list.add("小鸡鸡");
+        list.add("小鸡鸡");
+        list.add("小鸡鸡");
+        list.add("小鸡鸡");
+        list.add("小鸡鸡");
+        for (String s : list) {
+            System.out.println("我是龙增小鸡鸡，我对世界很好奇");
+        }
+    }
+}

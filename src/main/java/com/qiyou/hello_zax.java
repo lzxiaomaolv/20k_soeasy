0a5c23c71727aca92571590c24046352bcd5cb78
zax <zax1314157@163.com>
2019-07-26 08:56:40 +0800

    22222

--- src/main/java/com/qiyou/hello_zax.java
+++ src/main/java/com/qiyou/hello_zax.java
@@ -2,6 +2,6 @@ package com.qiyou;
 
 public class hello_zax {
     public static void main(String[] args) {
-        System.out.println("你好，小丁丁喜欢蜜桃臀");
+        System.out.println("你好，小丁丁喜欢蜜桃");
     }
 }

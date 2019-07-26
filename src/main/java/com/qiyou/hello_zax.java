43e6122aacaa570e5291877ede681108f045c857
zax <zax1314157@163.com>
2019-07-26 08:49:19 +0800

    不管了小丁丁喜欢蜜大爷

--- src/main/java/com/qiyou/hello_zax.java
+++ src/main/java/com/qiyou/hello_zax.java
@@ -2,6 +2,6 @@ package com.qiyou;
 
 public class hello_zax {
     public static void main(String[] args) {
-        System.out.println("你好，小鸡鸡");
+        System.out.println("你好，小丁丁喜欢蜜桃臀");
     }
 }

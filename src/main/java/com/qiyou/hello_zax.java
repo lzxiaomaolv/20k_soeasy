220c8b04eb830656ac1426e2cd94f03de7057679
longzeng <lz18508477338@163.com>
2019-07-26 09:13:21 +0800

    龙增在hello_zax.java新增了内容

--- src/main/java/com/qiyou/hello_zax.java
+++ src/main/java/com/qiyou/hello_zax.java
@@ -3,5 +3,6 @@ package com.qiyou;
 public class hello_zax {
     public static void main(String[] args) {
         System.out.println("你好，小丁丁喜欢蜜桃臀");
+        System.out.println("你好，zax喜欢蜜桃臀");
     }
 }
